24aa8d5d9810ad30569ced5e9d6572125ac375e4
dmzh1204 <94603460+dmzh1204@users.noreply.github.com>
2023-04-18 14:30:22 +0700

    wip

---
+++ index.js
@@ -0,0 +1,41 @@
+﻿const buildUrl = 'Build';
+const loaderUrl = buildUrl + '/barbarian_bg_vk.loader.js';
+
+const instanceConfig = {
+    dataUrl: buildUrl + '/barbarian_bg_vk.data',
+    frameworkUrl: buildUrl + '/barbarian_bg_vk.framework.js',
+    
+    codeUrl: buildUrl + '/barbarian_bg_vk.wasm',
+    
+    
+    
+    streamingAssetsUrl: 'StreamingAssets',
+    companyName: "DefaultCompany",
+    productName: "WebVkAuth",
+    productVersion: "0.1",
+    showBanner: () => {}
+}
+
+const onWindowLoaded = () => {
+    const loadingProgress = new progressBar('loading-progress');
+    const gameCanvas = document.getElementById('game-canvas');
+    const loaderScript = document.createElement('script');
+    
+    loaderScript.src = loaderUrl;
+    loadingProgress.setMax(1);
+    loadingProgress.show();
+    
+    loaderScript.onload = () => {
+        createUnityInstance(gameCanvas, instanceConfig, (progress) => loadingProgress.setCurrent(progress))
+            .then((unityInstance) => {
+                loadingProgress.hide();
+            })
+            .catch((msg) => {
+                alert(msg);
+            });
+    }
+    
+    document.body.appendChild(loaderScript);
+}
+
+window.onload = onWindowLoaded;
